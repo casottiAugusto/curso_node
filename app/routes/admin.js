f83caa69d764e3c66eec3a29c35c5fdc3e3bda4e
Augusto Note Dropship <augustocasottinegocio@gmail.com>
2024-02-24 11:28:55 -0300

    Validacao consertada

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,8 +1,19 @@
+const {body, validationResult}= require('express-validator')
 module.exports = function (app) {
   app.get("/admin", function (req, res) {
     app.app.controllers.admin.formulario_inclusao_notivia(app, req, res);
   });
-  app.post("/noticias/salvar", function (req, res) {
-    app.app.controllers.admin.noticias_salvar(app, req, res,validator);
+  app.post("/noticias/salvar",[
+    body('titulo')
+    .notEmpty()
+    .withMessage('Minimum 3 characters required!'),
+  ], function (req, res) {
+    
+    app.app.controllers.admin.noticias_salvar(app, req, res);
+    const erro =validationResult(req)
+    if (!erro.isEmpty()) {
+      console.log(erro.array())
+    }   
+    res.redirect('/noticias/salvar')
   });
 };

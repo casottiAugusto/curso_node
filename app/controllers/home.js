8a441ca6f16861e1469facd89dddba0b1aa0ba90
Augusto Note Dropship <augustocasottinegocio@gmail.com>
2024-03-02 17:25:12 -0300

    back-end1finalizado

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,5 +4,6 @@ module.exports.index = function(app,req,res){
   var noticiasModel = new app.app.models.noticiasModel(connection);
   noticiasModel.get5ultimasNoticias(function(error, result){
     res.render("home/index",{noticias:result});
+    console.log(result);
   });
 }
